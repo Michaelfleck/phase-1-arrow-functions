83e7dcfc98fe7067d8c7b7120d2d282d9993a32d
mfleck <michael.j.fleck@gmail.com>
2021-10-16 14:13:12 -0500

    commit tree 555c35b2771264d31ce0aa14f09c441445d097fd

---
+++ index.js
@@ -0,0 +1,47 @@
+
+//arrow function divide
+let divide = (a = 2000, b = 100) => {
+    return a/b;
+}
+
+divide(2000, 100);
+
+//arrow function square
+
+let square = a => {
+    return a * a;
+}
+
+
+//arrow function add
+
+let add = (a,b) => {
+    return (a+b);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
